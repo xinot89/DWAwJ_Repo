ea14153c29bcb8e1b7a4d903fb2f11583aec054a
xinot89 <toni.pohjanheimo@gmail.com>
2024-03-04 12:07:12 +0200

    Refactored that functions checkexc1..... can be used on both, exercise 1&4. Also removed bug where required comment let sent form without required email.

--- ws2-9/js/shopping.js
+++ ws2-9/js/shopping.js
@@ -1,77 +1,89 @@
-function checkexc1_s1() {
-    emailvalue = document.forms[0].email.value;
+//checkexc1 -functions refactored, so that first one takes form number as input and same functions are used in excercises 1&4
+
+function checkexc1_s1(tempvariable) {
+    which = parseInt(tempvariable);
+    emailvalue = document.forms[which].email.value;
     //emailvalue = "abaa@bba.12345";
     //console.log("Emailvalue: "+emailvalue);
-    commentvalue = document.forms[0].comment.value;
-    comment = document.forms[0].comment;
+    commentvalue = document.forms[which].comment.value;
+    comment = document.forms[which].comment;
     //Element type span in following create / "span" is not id.
-    //var textSpan = document.createElement('span');
-    //textSpan.className = "ws5form_errormsgs";
-    /*Inspiration: https://stackoverflow.com/questions/2435525/best-practice-access-form-elements-by-html-id-or-name-attribute*/
     checkexc1_email();
 }
 function checkexc1_email() {
     //let's clear potential error first, in case of previous error message:
-    document.getElementById("emailerroroutput").innerHTML = "";
+    document.getElementById("emailerroroutput"+which).innerHTML = "";
     if (emailvalue.length < 15 && emailvalue.length >5 && emailvalue.includes("@")) {
         //console.log("emailvalue riitti");
-        checkexc1_comment();
+        checkexc1_comment(1);
     } else if (emailvalue.length > 14) {
     console.log("Email too long");
-    document.forms[0].email.style.background ="rgba(255, 0, 0, 0.3)";
-    document.getElementById("emailerroroutput").innerHTML = "Email too long";
+    document.forms[which].email.style.background ="rgba(255, 0, 0, 0.3)";
+    document.getElementById("emailerroroutput"+which).innerHTML = "Email too long";
     //comment.parentNode.insertBefore(textSpan, comment.nextSibling);
     //comment.parentNode.insertBefore(textSpan, comment.previousSibling);
     event.preventDefault();
-    checkexc1_comment();
+    checkexc1_comment(0);
     } else if (emailvalue.length < 6){
         console.log("Email too short");
-        document.forms[0].email.style.background ="rgba(255, 0, 0, 0.3)";
-        document.getElementById("emailerroroutput").innerHTML = "Email too short";
+        document.forms[which].email.style.background ="rgba(255, 0, 0, 0.3)";
+        document.getElementById("emailerroroutput"+which).innerHTML = "Email too short";
         event.preventDefault();
-        checkexc1_comment();
+        checkexc1_comment(0);
     } else if (emailvalue.includes("@") == 0) {
         console.log("@ symbol not found");
-        document.forms[0].email.style.background ="rgba(255, 0, 0, 0.3)";
-        document.getElementById("emailerroroutput").innerHTML = "No required @ -symbol found";
+        document.forms[which].email.style.background ="rgba(255, 0, 0, 0.3)";
+        document.getElementById("emailerroroutput"+which).innerHTML = "No required @ -symbol found";
         event.preventDefault();
-        checkexc1_comment();
+        checkexc1_comment(0);
     } else {
         console.log("Input checking broken while checking email.");
-        document.forms[0].email.style.background ="rgba(255, 0, 0, 0.3)";
-        document.getElementById("emailerroroutput").innerHTML = "Input checking broken while checking email.";
+        document.forms[which].email.style.background ="rgba(255, 0, 0, 0.3)";
+        document.getElementById("emailerroroutput"+which).innerHTML = "Input checking broken while checking email.";
         event.preventDefault();
-        checkexc1_comment();
+        checkexc1_comment(0);
     }
 }
-function checkexc1_comment() {
-    document.getElementById("commenterroroutput").innerHTML = "";
+function checkexc1_comment(tempvrb) {
+    greenlight = parseInt(tempvrb);
+    document.getElementById("commenterroroutput"+which).innerHTML = "";
     //Check that comment field is not empty
     if (commentvalue.length > 0) {
         //Ei tyhjän kommenttikentän touhut
         if (commentvalue.length > 50) {
             //Works: console.log("Siivu commentvaluesta: " + commentvalue.slice(0,50))
-            document.forms[0].comment.value = String(commentvalue.slice(0,50));
-            commentvalue = document.forms[0].comment.value;
-            alert("Given email: " + emailvalue);
-            alert("Given comment: " + commentvalue);
+            document.forms[which].comment.value = String(commentvalue.slice(0,50));
+            commentvalue = document.forms[which].comment.value;
+            if (greenlight == 1) {
+                //Operations for succesful truncated comment with emailvalue verification also succeeded.
+                alert("Given email: " + emailvalue);
+                alert("Given comment: " + commentvalue);
+            }
         } else {
-            alert("Given email: " + emailvalue);
-            alert("Given comment: " + commentvalue);
+            if (greenlight == 1) {
+                ////Operations for succesful comment with emailvalue verification also succeeded.
+                alert("Given email: " + emailvalue);
+                alert("Given comment: " + commentvalue);
+            }
         }
     } else {
-        document.forms[0].comment.style.background ="rgba(255, 0, 0, 0.3)";
-        document.getElementById("commenterroroutput").innerHTML = "Comment required";
+        document.forms[which].comment.style.background ="rgba(255, 0, 0, 0.3)";
+        document.getElementById("commenterroroutput"+which).innerHTML = "Comment required";
         event.preventDefault();
     }
 }
 function exc1_towhite(which) {
-    
+    //"email"&"comment":Exercise 1, "email2"&"comment2":exercise4
     if (which=="email") {
         document.forms[0].email.style.background ="rgb(255, 255, 255)";
     } else if (which=="comment") {
         //console.log("kommentinvalkaisu laukesi");
         document.forms[0].comment.style.background ="rgb(255, 255, 255)";
+    } else if (which=="email2") {
+        document.forms[1].email.style.background ="rgb(255, 255, 255)";
+    } else if (which=="comment2") {
+        //console.log("kommentinvalkaisu laukesi");
+        document.forms[1].comment.style.background ="rgb(255, 255, 255)";
     } else {
         console.log("exc1_towhite didn't do anything.")
     }
@@ -124,17 +136,43 @@ function checkEligibility() {
         console.log("Nothing matched in checkElibility.")
     }
 }
-//Debugging function to help aiming text to right place:
-//Abandoned when figured out much simpler implementation technic shown on checkEligibility()
-/*function roiskiTekstia() {
-    years = parseInt(document.forms[1].years.value);
-    console.log("Years value: " + years);
-    var formElement = document.getElementById('exc2Years');
-    console.log("formElement asetettu: " + formElement);
-    var newText = document.createTextNode('Your Appended Text');
-    console.log("Muuttuja uudelle tekstille asetettu: " + newText);
-    var newSpan = document.createElement('span');
-    newSpan.appendChild(newText);
-    formElement.parentNode.insertBefore(newSpan, formElement.nextSibling);
+function calculate() {
+    //console.log(document.getElementById('quantity').value);
+    shippingpricearray = [30,45,55,85];
+    qty = parseInt(document.getElementById('quantity').value);
+    price = parseFloat(document.getElementById('price').value);
+    tax = parseFloat(document.getElementById('tax').value);
+    discount = parseFloat(document.getElementById('discount').value);
+    shipping = parseInt(document.getElementById('shipping').value);
+    sum = 0;
+    if (100 < qty) {
+        discount = discount*2;
+    }
+    //Check that everything can be found:
+    if (isNaN(qty)) {
+        alert("Please fill correct quantity");
+    } else if (isNaN(price)) {
+        alert("Please fill correct price");
+    } else if (isNaN(tax)) {
+        alert("Please fill correct VAT (%)");
+    } else if (isNaN(discount)) {
+        alert("Please fill correct Discount (%)");
+    } else if (isNaN(shipping)) {
+        alert("Please select shipping option");
+    } else {
+        //console.log("qty: " + qty);
+        //console.log("price: " + price);
+        //console.log("tax%: " + tax);
+        //console.log("discount: "+ discount);
+        //console.log("Shipping option: " + shipping);
+        sum = qty*price*((tax/100)+1)*(1-(discount/100))+shippingpricearray[shipping];
+        sum = sum.toFixed(2);
+        document.getElementById('total').value=sum;
+    }
+
+
+
+
+    //Discount when quantity > 100
+    //Add shipping
 }
-*/
